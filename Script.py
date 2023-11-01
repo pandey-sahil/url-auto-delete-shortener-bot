9bcba97daffe405d4520f22566e298ad0ee1ca27
Sahil Pandey <85783436+pandey-sahil@users.noreply.github.com>
2023-11-01 10:14:08 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,17 +12,17 @@ def is_enabled(value, default):
 
 class script(object):
     HOME_BUTTONURL_UPDATES = environ.get("HOME_BUTTONURL_UPDATES", 'https://omegalinks.in/')
-    START_TXT = environ.get("START_TXT", '''<b>​🇰​​🇴​​🇳​​🇮​​🇨​​🇭​​🇮​​🇼​​🇦​ {}-​🇸​​🇦​​🇳​ 👋🏻 
-                            ​🇮​'​🇲​ ​🇫​​🇮​​🇱​​🇪​ ​🇸​​🇭​​🇦​​🇷​​🇮​​🇳​​🇬​ ​🇧​​🇴​​🇹​
+    START_TXT = environ.get("START_TXT", '''<b>ᴋᴏɴɪᴄʜɪᴡᴀ​ {}-ꜱᴀɴ​ 👋🏻 
+                            ​ɪ'ᴍ ɴɪᴄᴏ ʀᴏʙɪɴ ᴀ ꜰɪʟᴇ ꜱʜᴀʀɪɴɢ ʙᴏᴛ
                             <a href=https://t.me/Uncensored_Hanimes><b>ᴜɴᴄᴇɴꜱᴏʀᴇᴅ ʜᴀɴɪᴍᴇꜱ</b> 😁.</b>''')
     HELP_TXT = """ʜᴇʏᴀ {}
 ʏᴏᴜ ᴡᴀɴᴛ ʜᴇʟᴘ ʜᴇʀᴇ ɪᴛ ɪꜱ ᴄʟɪᴄᴋ ʙᴇʟᴏᴡ."""
-    ABOUT_TXT = """<b><i>​🇲​​🇾​ ​🇳​​🇦​​🇲​​🇪​: <a href=https://t.me/NicoRobinRobot><b>🇳​​🇮​​🇨​​🇴​ ​🇷​​🇴​​🇧​​🇮​​🇳​</b></a>\n
-📝 ​🇱​​🇦​​🇳​​🇬​​🇺​​🇦​​🇬​​🇪​ : ᴘʏʀᴏɢʀᴀᴍ\n
-📚 🇫​​🇷​​🇦​​🇲​​🇪​​🇼​​🇴​​🇷​​🇰​ : ᴘʏᴛʜᴏɴ 3\n
-📡 ​🇭​​🇴​​🇸​​🇹​​🇪​​🇩​ ​🇴​​🇳​ : Koyeb\n
-📢 ​🇨​​🇭​​🇦​​🇳​​🇳​​🇪​​🇱​ : <a href=https://t.me/Uncensored_Hanimes><b></b>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n
-🌟 ​🇻​​🇪​​🇷​​🇸​​🇮​​🇴​​🇳 : ᴠ 4.0\n</b></i>"""
+    ABOUT_TXT = """<b><i>​ᴍʏ ɴᴀᴍᴇ​: <a href=https://t.me/NicoRobinRobot><b>ɴɪᴄᴏ ʀᴏʙɪɴ​</b></a>\n
+📝 ​ʟᴀɴɢᴜᴀɢᴇ​ : ᴘʏʀᴏɢʀᴀᴍ\n
+📚 ​ꜰʀᴀᴍᴇᴡᴏʀᴋ : ᴘʏᴛʜᴏɴ 3\n
+📡 ​ʜᴏsᴛᴇᴅ ᴏɴ : Koyeb\n
+📢 ​ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/Uncensored_Hanimes><b></b>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a>\n
+🌟 ​ᴠᴇʀsɪᴏɴ : ᴠ 4.0\n</b></i>"""
     SOURCE_TXT = """<b>......</b>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
